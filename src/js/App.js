54aecdfa9d84a8c00cc052e048810e5f84ddcea8
Arnar Kari <arnarkari93@gmail.com>
2015-02-13 22:49:07 -0800

    update path for views

--- src/js/App.js
+++ src/js/App.js
@@ -3,9 +3,9 @@ var ChatClient = angular.module('ChatClient', ['ngRoute']);
 ChatClient.config(
 	function ($routeProvider) {
 		$routeProvider
-			.when('/login', { templateUrl: 'Views/login.html', controller: 'LoginController' })
-			.when('/rooms/:user/', { templateUrl: 'Views/rooms.html', controller: 'RoomsController' })
-			.when('/room/:user/:room/', { templateUrl: 'Views/room.html', controller: 'RoomController' })
+			.when('/login', { templateUrl: 'src/Views/login.html', controller: 'LoginController' })
+			.when('/rooms/:user/', { templateUrl: 'src/Views/rooms.html', controller: 'RoomsController' })
+			.when('/room/:user/:room/', { templateUrl: 'src/Views/room.html', controller: 'RoomController' })
 			.otherwise({
 	  			redirectTo: '/login'
 			});
