e02f0626c2452529c33d36c38670b2d583b71329
Arnar Kari <arnarkari93@gmail.com>
2015-02-15 18:32:43 -0800

    Added createNewRomm function

--- src/js/App.js
+++ src/js/App.js
@@ -24,6 +24,7 @@ ChatClient.controller('LoginController', function ($scope, $location, $rootScope
 			socket.emit('adduser', $scope.nickname, function (available) {
 				if (available) {
 					$location.path('/rooms/' + $scope.nickname);
+
 				} else {
 					$scope.errorMessage = 'This nick-name is already taken!';
 				}
@@ -34,8 +35,41 @@ ChatClient.controller('LoginController', function ($scope, $location, $rootScope
 
 ChatClient.controller('RoomsController', function ($scope, $location, $rootScope, $routeParams, socket) {
 	// TODO: Query chat server for active rooms
-	$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+	$scope.errorMessage = '';
 	$scope.currentUser = $routeParams.user;
+	$scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+	$scope.createNewRoom = function() {
+		$scope.newRoom = {
+			room : 'GreenRoom',
+			pass : undefined
+		};
+		socket.emit('joinroom', $scope.newRoom, function (success, reason) {
+			if (!success) {
+				$scope.errorMessage = reason;
+			} else {
+				console.log($scope.newRoom.room);
+				$location.path('/room/' + $routeParams.user + '/' + $scope.newRoom.room);
+			}
+
+		});
+	}
+
+	// socket.on('rooms', function () {
+	// 	socket.emit('roomlist', $scope.rooms, function (availableRooms) {
+	// 		console.log("inni rooms socket");
+
+	// 		$scope.$apply(function(){
+	// 			if (availableRooms) {
+	// 				console.log(availableRooms);
+	// 				$scope.rooms = availableRooms;
+	// 			} else {
+	// 				console.log("no rooms");
+	// 				$scope.errorMessage = "There are no available rooms";
+	// 			}
+	// 		});
+	// 	});
+	// });
+	// $scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
 });
 
 ChatClient.controller('RoomController', function ($scope, $location, $rootScope, $routeParams, socket) {
