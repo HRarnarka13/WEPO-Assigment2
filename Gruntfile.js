de283a64fc052b3a7741c8dd68f3bd80738b60bf
Arnar Kari <arnarkari93@gmail.com>
2015-02-15 21:10:06 -0800

    Added grunt task for concating js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,10 @@
 module.exports = function ( grunt ) { 
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.initConfig({
 		jshint: {
-			src: ['src/**/*.js'],
+			src: ['src/**/*.js', '!src/js/dest/output.min.js'],
 			gruntfile: ['Gruntfile.js'],
 			options: {
 				curly:  true,
@@ -28,12 +29,23 @@ module.exports = function ( grunt ) {
 		uglify: {
 			my_target: {
 		      	files: {
-		        	'src/js/dest/output.min.js': ['src/js/*.js']
+		        	'src/js/dest/output.min.js': ['src/js/dest/built.js']
 		      	}
 		    }
-		}
+		},
+		concat: {
+		    options: {
+		    	separator: ';',
+		    },
+		    dist: {
+				src: ['src/**/*.js', 'node_modules/socket.io/node_modules/socket.io-client/dist/socket.io.js'],
+		      	dest: 'src/js/dest/built.js',
+		    },
+		 }
 	});
 	grunt.registerTask('checkjs', ['jshint']);
 	grunt.registerTask('minify', ['uglify']);
+	grunt.registerTask('makeonefile', ['concat']);
+	grunt.registerTask('makejs', ['concat', 'uglify']);
 	grunt.registerTask('default', ['jshint', 'uglify']);
 };
