dcb515f8eba2e2754dc857f441163f7d3d28942e
Arnar Kari <arnarkari93@gmail.com>
2015-02-20 12:55:55 -0800

    use .currentUser insted of .user

--- src/js/controllers/RoomsController.js
+++ src/js/controllers/RoomsController.js
@@ -21,26 +21,26 @@ function ($scope, $location, $rootScope, $routeParams, socket) {
 			if (!success) {
 				$scope.errorMessage = reason;
 			} else {
-				$location.path('/room/' + $routeParams.user + '/' + $scope.newRoom.room);
+				$location.path('/room/' + $scope.currentUser + '/' + $scope.newRoom.room);
 			}
 
 		});
 	};
 
-	// socket.on('rooms', function () {
-	// 	socket.emit('roomlist', $scope.rooms, function (availableRooms) {
-	// 		console.log("inni rooms socket");
+	socket.on('rooms', function () {
+		socket.emit('roomlist', $scope.rooms, function (availableRooms) {
+			console.log("inni rooms socket");
 
-	// 		$scope.$apply(function(){
-	// 			if (availableRooms) {
-	// 				console.log(availableRooms);
-	// 				$scope.rooms = availableRooms;
-	// 			} else {
-	// 				console.log("no rooms");
-	// 				$scope.errorMessage = "There are no available rooms";
-	// 			}
-	// 		});
-	// 	});
-	// });
+			$scope.$apply(function(){
+				if (availableRooms) {
+					console.log(availableRooms);
+					$scope.rooms = availableRooms;
+				} else {
+					console.log("no rooms");
+					$scope.errorMessage = "There are no available rooms";
+				}
+			});
+		});
+	});
 	// $scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
 });
