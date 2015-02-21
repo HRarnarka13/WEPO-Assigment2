4e16ac28aac8e2bd2ed3c4c061417945a28ca8e4
Arnar Kari <arnarkari93@gmail.com>
2015-02-21 11:09:08 -0800

    rooms and message stuff

--- src/js/controllers/RoomController.js
+++ src/js/controllers/RoomController.js
@@ -3,21 +3,28 @@ function ($scope, $location, $rootScope, $routeParams, socket) {
 	$scope.currentRoom = $routeParams.room;
 	$scope.currentUser = $routeParams.user;
 	$scope.currentUsers = [];
+	$scope.messageHistory = [];
 	$scope.errorMessage = '';
 
 	$scope.sendMessage = {
 		roomName: $scope.currentRoom,
 		msg: $scope.message
-	}
-	console.log($scope.sendMessage);
+	};
+
+	$scope.newRoom = {
+			room : $scope.currentRoom,
+			pass : undefined
+	};
+
+	console.log("$scope.sendMessage: " + $scope.sendMessage);
 
 	$scope.sendMsg = function(){
 		$scope.sendMessage.msg = $scope.message;
-		console.log($scope.sendMessage);
+		console.log("Message: " + $scope.sendMessage.msg);
 		socket.emit('sendmsg', $scope.sendMessage, function (message){
 
 		});
-
+		$scope.getMessages();
 		// socket.emit('addMessage', $scope.message, function (Message){
 		// });
 	};
@@ -26,19 +33,42 @@ function ($scope, $location, $rootScope, $routeParams, socket) {
 		$location.path('/rooms/' + $scope.nickname);
 	};
 
+	socket.emit('joinroom', $scope.newRoom, function (success, reason) {
+		if (!success) {
+			$scope.errorMessage = reason;
+		}
+	});
+
 	socket.on('updateusers', function (roomName, users, ops) {
 		// TODO: Check if the roomName equals the current room !
+		console.log("updateusers: " + users);
 		$scope.currentUsers = users;
-	});		
-
-	socket.emit("rooms", function() {
-		console.log("inni rooms socket");
 	});
 
-	socket.on("roomlist", function(roomList) {
-		$scope.messageHistory = roomList.lobby.messageHistory;
+	socket.on('updatechat', function (roomName, messages) {
+		console.log("updatechat, messages");
+		console.log(messages);
+		$scope.$apply = function () {
+			for (var i = messages.length - 1; i >= 0; i--) {
+				$scope.messageHistory.push(messages[i].message);
+			};
+		}
+		console.log("scope.messageHistory");
 		console.log($scope.messageHistory);
 	});
+
+	// $scope.getMessages = function () {
+	// 	socket.emit("rooms", function() {
+	// 		console.log("inni rooms socket");
+	// 	});
+	// };
+
+	// socket.on("roomlist", function(roomList) {
+	// 	console.log(roomList);
+	// 	$scope.messageHistory = roomList.lobby.messageHistory;
+	// 	console.log('Message History: ');
+	// 	console.log($scope.messageHistory);
+	// });
 
 	/*socket.emit('joinroom', $scope.currentRoom, function (success, reason) {
 		if (!success) {
--- src/js/controllers/RoomsController.js
+++ src/js/controllers/RoomsController.js
@@ -8,52 +8,22 @@ function ($scope, $location, $rootScope, $routeParams, socket) {
 			$scope.createNewRoom();
 		}
 	});
-	
 	$scope.errorMessage = '';
-	// $scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
+
 	$scope.createNewRoom = function() {
 		if ($scope.roomName === '') {
 			return;
 		}
-		$scope.newRoom = {
-			room : $scope.roomName,
-			pass : undefined
-		};
-		socket.emit('joinroom', $scope.newRoom, function (success, reason) {
-			if (!success) {
-				$scope.errorMessage = reason;
-			} else if ($scope.roomName !== "undefined") {
-				$location.path('/room/' + $scope.currentUser + '/' + $scope.roomName);
-			}
-
-		});
+		$location.path('/room/' + $scope.currentUser + '/' + $scope.roomName);
 	};
+
 	socket.emit("rooms", function () {
 		console.log("inni rooms socket");
 	});
 
 	socket.on("roomlist", function(roomList) {
-			console.log("inni roomlist socket");
-			console.log(Object.keys(roomList));
-			$scope.rooms = Object.keys(roomList);
+		console.log("inni roomlist socket");
+		console.log(Object.keys(roomList));
+		$scope.rooms = Object.keys(roomList);
 	});
-
-	// console.log($scope.rooms);
-	// //socket.on('rooms', function () {
-	// $scope.getRooms = function () {
-	// 	socket.emit('roomlist', $scope.rooms, function (availableRooms) {
-	// 		console.log("inni rooms socket");
-	// 		$scope.$apply(function(){
-	// 			if (availableRooms) {
-	// 				console.log(availableRooms);
-	// 				$scope.rooms = availableRooms;
-	// 			} else {
-	// 				console.log("no rooms");
-	// 				$scope.errorMessage = "There are no available rooms";
-	// 			}
-	// 		});
-	// 	});
-	// };
-	// $scope.getRooms();
-	// $scope.rooms = ['Room 1','Room 2','Room 3','Room 4','Room 5'];
 });
