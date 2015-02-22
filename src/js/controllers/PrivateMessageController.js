1fe2237aec05fce51ee3dbcb2eb2b876707fc3e4
Arnar Kari <arnarkari93@gmail.com>
2015-02-21 18:52:13 -0800

    more private messages

--- src/js/controllers/PrivateMessageController.js
+++ src/js/controllers/PrivateMessageController.js
@@ -6,10 +6,9 @@ ChatClient.controller('PrivateMessageController', [
 	'socket',
 function ($scope, $location, $rootScope, $routeParams, socket) {
 	console.log("private");
-	$scope.nick = "HAHAH";
 	$scope.users = [];
-
-	
+	$scope.privateMessage = '';
+	$scope.privateMessages = [];
 
 	$scope.getUsers = function () {
 		socket.emit('users');
@@ -17,5 +16,25 @@ function ($scope, $location, $rootScope, $routeParams, socket) {
 		socket.on('userlist', function (userlist) {
 			$scope.users = userlist;
 		});
-	}
+	};
+
+	$scope.startChat = function (user) {
+		$scope.friend = user;
+	};
+
+	$scope.sendPrivateMessage = function (user, message) {
+		$scope.privateMessage = '';
+		$scope.privateMessages.push(message);
+		console.log("To: " + user + " Message: " + message);
+		socket.emit('privatemsg', {nick: user, message: message}, function(sent) {
+			if (sent) {
+				console.log("private message sent");
+			}
+		});
+	};
+
+	 socket.on('recv_privatemsg', function(friend, message) {
+	 	$scope.recvMessage = message;
+	 });
+
 }]);
