7d3a59a89a399ee08bafb6e33bd9dbe1b0248fa8
Arnar Kari <arnarkari93@gmail.com>
2015-02-24 15:03:29 -0800

    Cleanup and commit

--- client/js/controllers/RoomsController.js
+++ client/js/controllers/RoomsController.js
@@ -5,30 +5,30 @@ angular.module("ChatClient").controller('RoomsController', [
 	'$routeParams',
 	'socket',
 function ($scope, $location, $rootScope, $routeParams, socket) {
-	// TODO: Query chat server for active rooms
+
 	$scope.currentUser = $routeParams.user;
 	$scope.errorMessage = '';
 
-	socket.emit("rooms", function () {
-		console.log("inni rooms socket");
-	});
+	socket.emit("rooms");
 
+	// Get list of active rooms
 	socket.on("roomlist", function(roomList) {
-		console.log("inni roomlist socket");
-		console.log(Object.keys(roomList));
 		$scope.rooms = Object.keys(roomList);
 	});
 
+	// Join a given room, if the roomName is undefined the user is creating a new room
 	$scope.joinRoom = function(roomName) {
-		console.log('joinRoom');
+
 		if (roomName === undefined) {
 			if ($scope.roomName === '' || $scope.roomName === undefined) {
 				$scope.errorMessage = 'room name is empty!';
 				return;
 			} else {
+				// set currentRoom the new given room in input field
 				$scope.currentRoom = $scope.roomName;
 			}
 		} else {
+			// set currentRoom from the given parameter
 			$scope.currentRoom = roomName;
 		}
 		socket.emit('joinroom', {room: $scope.currentRoom, pass: ''}, function (success, reason) {
